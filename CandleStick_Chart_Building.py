20179d4cc81c93b869cc6adc0f8b1e56fb54f34a
Groza Mihai <grozam@ArchLinux.localdomain>
2019-09-07 22:23:22 -0400

    changed libraries and fixed outdated code

--- CandleStick_Chart_Building.py
+++ CandleStick_Chart_Building.py
@@ -1,10 +1,12 @@
 import matplotlib.pyplot as plt
 from matplotlib import style
-from matplotlib.finance import candlestick_ohlc
 import matplotlib.dates as mdates
-import quandl
+import mpl_finance as mpl
 from tkinter import *
-style.use("ggplot")
+from yahoo_fin.stock_info import get_data
+import pandas as pd
+import plotly.graph_objects as go
+
 
 class AutoPlot:
     def __init__(self):
@@ -12,7 +14,7 @@ class AutoPlot:
         Label(master, text="Stock Ticker").grid(row=0)
         Label(master, text="Range Start").grid(row=1)
         Label(master, text="Range End").grid(row=2)
-        
+       
         self.e1 = Entry(master)
         self.e2 = Entry(master)
         self.e3 = Entry(master)
@@ -29,28 +31,19 @@ class AutoPlot:
 
     def make_plot(self):
         #Extracting data
-        df = quandl.get("WIKI/{ticker}".format(ticker=self.e1.get()),
-                        trim_start = self.e2.get(), trim_end = self.e3.get())
+        df = get_data("{ticker}".format(ticker=self.e1.get()),
+                        start_date = self.e2.get(), end_date = self.e3.get())
         
-        #Resampling the data set at a 10 day frequency
-        df_ohlc = df["Adj. Close"].resample("10D").ohlc()
-        df_volume = df["Volume"].resample("10D").sum()
-        
-        df_ohlc.reset_index(inplace=True)
-        
-        #Converting Date to mdate format
-        df_ohlc["Date"]= df_ohlc["Date"].map(mdates.date2num)
-        
-        #print(df_ohlc.head())
-        
-        #Plotting
-        ax1 = plt.subplot2grid((6,1), (0,0), rowspan=5, colspan=1)
-        ax2 = plt.subplot2grid((6,1),(5,0), rowspan=1, colspan=1, sharex=ax1)
-        ax1.xaxis_date()
-        
-        candlestick_ohlc(ax1, df_ohlc.values, width=2, colorup="g")
-        ax2.fill_between(df_volume.index.map(mdates.date2num),df_volume.values, 0)
-        plt.show()
-        plt.tight_layout()
-        
+
+        df.index = pd.to_datetime(df.index)
+
+        fig = go.Figure(data=[go.Candlestick(x=df.index,
+                open=df['open'],
+                high=df['high'],
+                low=df['low'],
+                close=df['close'])])
+
+        fig.show()
+
+     
 AutoPlot()
